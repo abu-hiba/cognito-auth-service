be6d562626331ed69b64416a4f8576f863c345f1
tombolton96 <tbolton96@icloud.com>
2022-10-21 13:14:07 +0100

    Remove unneccesary route

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ dotenv.config()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
 app.use('/auth', authRouter)
 
 app.listen(PORT, HOST, () => {
